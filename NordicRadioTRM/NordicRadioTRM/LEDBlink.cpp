137188413ba41a822d2a7297b13fa6595d2964e8
KellarHome <ckellar33@gmail.com>
2016-08-02 05:57:30 -0700

    Code clean up. Reset SAMPLES_IN_BUFFER to 2.

--- NordicRadioTRM/NordicRadioTRM/LEDBlink.cpp
+++ NordicRadioTRM/NordicRadioTRM/LEDBlink.cpp
@@ -66,7 +66,7 @@ static bool                    m_saadc_initialized = false;
 /* END Varibles for RTC */
 const nrf_drv_timer_t TIMER_LED = NRF_DRV_TIMER_INSTANCE(1);
 /* Variable Declarations for SAADC */
-#define SAMPLES_IN_BUFFER 1//5 - Having this set to 1 decreases current draw
+#define SAMPLES_IN_BUFFER 2//5
 
 static const nrf_drv_timer_t   m_timer = NRF_DRV_TIMER_INSTANCE(0);
 static nrf_saadc_value_t       m_buffer_pool[2][SAMPLES_IN_BUFFER];
@@ -425,28 +425,6 @@ void timer_handler(nrf_timer_event_t event_type, void* p_context)
 
 void saadc_sampling_event_init(void)
 {
-	//ret_code_t err_code;
-	//err_code = nrf_drv_ppi_init();
-	//APP_ERROR_CHECK(err_code);
-//
-	//err_code = nrf_drv_timer_init(&m_timer, NULL, timer_handler);
-	//APP_ERROR_CHECK(err_code);
-//
-	    ///* setup m_timer for compare event every 400ms */
-	////uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 400);
-	//uint32_t ticks = nrf_drv_timer_us_to_ticks(&m_timer, 63);
-	//nrf_drv_timer_extended_compare(&m_timer, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
-	//nrf_drv_timer_enable(&m_timer);
-//
-	//uint32_t timer_compare_event_addr = nrf_drv_timer_compare_event_address_get(&m_timer, NRF_TIMER_CC_CHANNEL0);
-	//uint32_t saadc_sample_event_addr = nrf_drv_saadc_sample_task_get();
-//
-	    ///* setup ppi channel so that timer compare event is triggering sample task in SAADC */
-	//err_code = nrf_drv_ppi_channel_alloc(&m_ppi_channel);
-	//APP_ERROR_CHECK(err_code);
-    //
-	//err_code = nrf_drv_ppi_channel_assign(m_ppi_channel, timer_compare_event_addr, saadc_sample_event_addr);
-	//APP_ERROR_CHECK(err_code);
 	ret_code_t err_code;
 	err_code = nrf_drv_ppi_init();
 	APP_ERROR_CHECK(err_code);
@@ -490,13 +468,7 @@ void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
      
 		err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
 		APP_ERROR_CHECK(err_code);
-
-		//int i;
-		//printf("ADC event number: %d\r\n", (int)m_adc_evt_counter);
-		//for (i = 0; i < SAMPLES_IN_BUFFER; i++)
-		//{
-			//printf("%d\r\n", p_event->data.done.p_buffer[i]);
-		//}
+		
 		m_adc_evt_counter++;
 		HandleTransmitState();
 	}
@@ -504,21 +476,6 @@ void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
 
 void saadc_init(void)
 {
-	//ret_code_t err_code;
-	//nrf_saadc_channel_config_t channel_config =
-	        //NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
-	//err_code = nrf_drv_saadc_init(NULL, saadc_callback);
-	//APP_ERROR_CHECK(err_code);
-//
-	//err_code = nrf_drv_saadc_channel_init(0, &channel_config);
-	//APP_ERROR_CHECK(err_code);
-//
-	//err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
-	//APP_ERROR_CHECK(err_code);
-    //
-	//err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[1], SAMPLES_IN_BUFFER);
-	//APP_ERROR_CHECK(err_code);
-	
 	ret_code_t err_code;
 	
 	nrf_saadc_channel_config_t channel_0_config =
